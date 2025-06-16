d89988f8d1b7b1961fbe842084aa2535d209f60c
stefannmikiic <mikicstefan1@gmail.com>
2025-06-16 17:03:25 +0200

    dinamicki unos u ponudu

---
+++ js/ponuda.js
@@ -0,0 +1,54 @@
+const proizvodi = [
+
+    {
+        id: 1,
+        slika:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Parmigiano_reggiano_piece.jpg/640px-Parmigiano_reggiano_piece.jpg",
+        naziv: "Parmezan",
+        opis: "Opis proizvoda",
+        staraCena: 1000,
+        novaCena: 900
+    },
+    {
+        id: 2,
+        slika:"https://thecheeseatlas.com/wp-content/uploads/2020/05/Mauri-Gorgonzola-Piccante-1024x819.jpeg",
+        naziv: "Gorgonzola",
+        opis: "Opis proizvoda",
+        staraCena: 800,
+        novaCena: 800
+    }
+
+
+]
+
+function prikaziProizvode(){
+    const elementDiv = document.getElementById("sviproizvodi");
+    elementDiv.innerHTML = "";
+
+    proizvodi.forEach(proizvod => {
+        const div = document.createElement("div");
+        div.className = "proizvod";
+
+        if(proizvod.staraCena !== proizvod.novaCena) {
+        div.innerHTML = `
+            <img src="${proizvod.slika}" alt="${proizvod.naziv}">
+            <h3>${proizvod.naziv}</h3>
+            <p>${proizvod.opis}</p>
+            <span class="staracena"><i class="fa-solid fa-tags"></i>Stara cena: ${proizvod.staraCena} RSD</span> <br>
+            <span class="novacena">Nova cena: ${proizvod.novaCena} RSD</span>
+            <button class="btn"><i class="fa-solid fa-cart-plus"></i>Dodaj u korpu</button>
+        `;
+        }
+        if(proizvod.staraCena === proizvod.novaCena) {
+            div.innerHTML = `
+                <img src="${proizvod.slika}" alt="${proizvod.naziv}">
+                <h3>${proizvod.naziv}</h3>
+                <p>${proizvod.opis}</p>
+                <span class="nova-cena">Cena: ${proizvod.novaCena} RSD</span>
+                <button class="btn"><i class="fa-solid fa-cart-plus"></i>Dodaj u korpu</button>
+            `;
+        }
+        elementDiv.appendChild(div);
+    })
+}
+
+prikaziProizvode();
